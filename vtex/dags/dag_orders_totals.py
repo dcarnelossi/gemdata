605a7f7a0d43df1a2120f14d09c43d1fdbd50cb7
Daniel Carnelossi <daniel.carnelossi@zetta.health>
2024-08-13 12:06:46 -0300

    orders totals

--- vtex/dags/dag_orders_totals.py
+++ vtex/dags/dag_orders_totals.py
@@ -7,6 +7,15 @@ from airflow.decorators import task
 from airflow.models import Variable
 from airflow.models.param import Param
 
+from modules.dags_common_functions import (
+    get_coorp_conection_info,
+    get_data_conection_info,
+    integrationInfo,
+    get_api_conection_info,
+    get_import_last_rum_date,
+)
+
+
 # Lista de requisitos
 requirements = [
     "openai==1.6.0",
@@ -42,124 +51,12 @@ with DAG(
         )
     },
 ) as dag:
-    from modules import dbpgconn
-
-    def integrationInfo(connection_info, integration_id):
-        try:
-            print("integrationInfo")
-
-            start_time = time.time()
-
-            # postgres_conn = dbpgconn.PostgresConnection(connection_info)
-
-            query = f"""SELECT *
-                        FROM public.integrations_integration
-                        WHERE id = '{integration_id}';"""
-
-            select = dbpgconn.WriteJsonToPostgres(connection_info, query)
-            result = select.query()
-
-            if result:
-                logging.info(
-                    f"Importação das BRANDS Concluída com sucesso. \
-                        Tempo de execução: {time.time() - start_time:.2f} segundos"
-                )
-                logging.info(f"Tempo de execução: {time.time() - start_time:.2f}")
-                print(result)
-                return (result,)
-            else:
-                logging.error(
-                    f"Importação das BRANDS deu pau. \
-                        Tempo de execução: {time.time() - start_time:.2f} segundos"
-                )
-                return False
-        except Exception as e:
-            logging.exception("An unexpected error occurred during BRANDS import" - e)
-            return False
-
-    def get_import_last_rum_date(connection_info, integration_id):
-        try:
-            print("get_import_last_rum_date")
-
-            # postgres_conn = dbpgconn.PostgresConnection(connection_info)
-
-            query = f"""SELECT import_last_rum_date
-                        FROM public.integrations_integration
-                        WHERE id = '{integration_id}';"""
-
-            select = dbpgconn.WriteJsonToPostgres(connection_info, query)
-            result = select.query()
-
-            if result:
-                return result
-            else:
-                logging.error("Importação das get_import_last_rum_date deu pau")
-                return False
-        except Exception as e:
-            logging.exception("An unexpected error occurred during BRANDS import" - e)
-            return e
-
-    def get_coorp_conection_info(integration_id):
-        coorp_conection_info = {
-            "host": Variable.get("COORP_PGHOST"),
-            "user": Variable.get("COORP_PGUSER"),
-            "port": 5432,
-            "database": Variable.get("COORP_PGDATABASE"),
-            "password": Variable.get("COORP_PGPASSWORD"),
-            "schema": "public",
-        }
-
-        return coorp_conection_info
-
-    def get_data_conection_info(integration_id):
-        data_conection_info = {
-            "host": Variable.get("DATA_PGHOST"),
-            "user": Variable.get("DATA_PGUSER"),
-            "port": 5432,
-            "database": Variable.get("DATA_PGDATABASE"),
-            "password": Variable.get("DATA_PGPASSWORD"),
-            "schema": integration_id,
-        }
-
-        return data_conection_info
-
-    def get_api_conection_info(integration_id):
-        try:
-            print(integration_id)
-
-            data = integrationInfo(
-                get_coorp_conection_info(integration_id), integration_id
-            )
-
-            api_conection_info = data[0][1][0]
-            # VTEX_API_AppKey = api_conection_info['vtex_api_appkey']
-
-            headers = {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "X-VTEX-API-AppKey": api_conection_info["vtex_api_appkey"],
-                "X-VTEX-API-AppToken": api_conection_info["vtex_api_apptoken"],
-            }
-
-            vtexapi = {
-                "VTEX_Domain": f"{api_conection_info['vtex_api_accountname']}.\
-                    {api_conection_info['vtex_api_environment']}.com.br",
-                "headers": headers,
-            }
-
-            return vtexapi
-
-        except Exception as e:
-            logging.exception(f"An unexpected error occurred during DAG - {e}")
-            raise e
-        finally:
-            pass
 
     @task(provide_context=True)
     def orders_totals(**kwargs):
         integration_id = kwargs["params"]["PGSCHEMA"]
 
-        coorp_conection_info = get_coorp_conection_info(integration_id)
+        coorp_conection_info = get_coorp_conection_info()
         data_conection_info = get_data_conection_info(integration_id)
         api_conection_info = get_api_conection_info(integration_id)
 
