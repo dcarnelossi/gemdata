2843115f0657095f94757f271c34300d909553a5
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-08-16 11:20:49 -0300

    a

--- vtex/dags/dag_start_update_daily2.py
+++ vtex/dags/dag_start_update_daily2.py
@@ -2,11 +2,11 @@ import logging
 from datetime import datetime
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from airflow.models.param import Param
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.utils.dates import days_ago
-from airflow.hooks.postgres_hook import PostgresHook
-from airflow.operators.python import ShortCircuitOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.task_group import TaskGroup
 
 # Lista de requisitos
 requirements = [
@@ -25,53 +25,67 @@ default_args = {
     "email_on_retry": False,
 }
 
-def get_customer_ids(**kwargs):
-    hook = PostgresHook(postgres_conn_id="appgemdata-dev")
-    sql = """   select distinct id from public.integrations_integration
-            where is_active = true 
-            and infra_create_status = true limit 2"""
-    ids = hook.get_records(sql)
-    ids = [str(id[0]) for id in ids]  # Transform list of tuples to list of strings
-    return ids
-
-# def check_dag_status(ti, triggered_dag_run, **kwargs):
-#     dag_run_status = triggered_dag_run.get_state()
-#     if dag_run_status == 'success':
-#         return True
-#     return False
-
+# Usando o decorator @dag para criar o objeto DAG
 with DAG(
-    "1-StartDaily",
+    "0-StartDaily",
     schedule_interval=None,
     catchup=False,
     default_args=default_args,
-    tags=["StartDaily", "v2", "trigger_dag_daily_update"],
-    
+    tags=["StartDaily", "v1", "trigger_dag_daily_update"],
+    render_template_as_native_obj=True,
+#    render_template_as_native_obj=True,
+    # params={
+    #     "PGSCHEMA": Param(
+    #         type="string",
+    #         title="PGSCHEMA:",
+    #         description="Enter the integration PGSCHEMA.",
+    #         section="Important params",
+    #         min_length=1,
+    #         max_length=200,
+    #     ),
+    #     "ISDAILY": Param(
+    #         type="boolean",
+    #         title="ISDAILY:",
+    #         description="Enter com False (processo total) ou True (processo diario) .",
+    #         section="Important params",
+    #         min_length=1,
+    #         max_length=10,
+    #     )
+    # },
 ) as dag:
 
-        get_ids = PythonOperator(
-            task_id='get_customer_ids',
-            python_callable=get_customer_ids,
-            provide_context=True,
-        )
-        a=get_ids.output
-        print(get_ids.output)
-        for customer_id in a:
-            trigger_dag = TriggerDagRunOperator(
-                    task_id=f'trigger_dag_imports_{a}',
-                    trigger_dag_id='1-ImportVtex-Brands-Categories-Skus-Products',
-                    conf={
-                        "PGSCHEMA": a,
-                        "ISDAILY": False
-                    },
-                
-                )
+    @task(provide_context=True)
+    def get_integration_ids(**kwargs):
+        try:
+            # Conecte-se ao PostgreSQL e execute o script
+            hook = PostgresHook(postgres_conn_id="appgemdata-dev")
+            query = """
+            select distinct id from public.integrations_integration
+            where is_active = true 
+            and infra_create_status = true limit 2
+            """
+            integration_ids = hook.get_records(query)
+            # Armazena os IDs no contexto
+            kwargs['ti'].xcom_push(key='integration_ids', value=[integration[0] for integration in integration_ids])
 
-                # wait_for_dag = ShortCircuitOperator(
-                #     task_id=f'wait_for_dag_for_customer_{customer_id}',
-                #     python_callable=check_dag_status,
-                #     op_args=[trigger_dag],
-                #     provide_context=True,
-                # )
+        except Exception as e:
+            logging.exception(
+                f"An unexpected error occurred during get_integration_ids - {e}"
+            )
 
-            get_ids >> trigger_dag #>> wait_for_dag
+    @task(provide_context=True)
+    def trigger_import_dags(**kwargs):
+        # Recupera os IDs do contexto
+        integration_ids = kwargs['ti'].xcom_pull(key='integration_ids')
+        for integration_id in integration_ids:
+            TriggerDagRunOperator(
+                task_id=f"trigger_dag_imports_{integration_id}",
+                trigger_dag_id="1-ImportVtex-Brands-Categories-Skus-Products",
+                conf={
+                    "PGSCHEMA": integration_id,
+                    "ISDAILY": False
+                }
+            ).execute(context=kwargs)
+
+    # Definir a ordem das tarefas
+    get_integration_ids() >> trigger_import_dags()
