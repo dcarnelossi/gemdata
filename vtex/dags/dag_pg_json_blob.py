7e7e4755527005d1b374ea4326cab95092b01fc2
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-08-16 19:58:16 -0300

    a

--- vtex/dags/dag_pg_json_blob.py
+++ vtex/dags/dag_pg_json_blob.py
@@ -88,7 +88,7 @@ def extract_postgres_to_json():
 # Função para mover o arquivo JSON para o diretório no Blob Storage
 def upload_to_blob_directory(ti):
     output_filepath = ti.xcom_pull(task_ids='extract_postgres_to_json')
-    wasb_hook = WasbHook(wasb_conn_id='azure_blob_storage_json')
+  #  wasb_hook = WasbHook(wasb_conn_id='azure_blob_storage_json')
     blob_name= f'{PGSCHEMA}/postgres_data2.json',
         # Verifica se o arquivo já existe
     #if wasb_hook.check_for_blob(container_name="jsondashboard", blob_name=blob_name):
@@ -112,7 +112,7 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     default_args=default_args,
-    tags=["jsonblob", "v2", "ALTERAR"],
+    tags=["jsonblob", "v1", "ALTERAR"],
 
 ) as dag:
     
