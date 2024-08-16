5658c3b80da53a6f6d76e81026f48ce24adda8e8
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-08-16 11:46:26 -0300

    a

--- vtex/dags/dag_start_update_daily2.py
+++ vtex/dags/dag_start_update_daily2.py
@@ -3,10 +3,9 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.decorators import task
-from airflow.models.param import Param
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.utils.task_group import TaskGroup
+from airflow.api.common.experimental.trigger_dag import trigger_dag
 
 # Lista de requisitos
 requirements = [
@@ -27,7 +26,7 @@ default_args = {
 
 # Usando o decorator @dag para criar o objeto DAG
 with DAG(
-    "0-StartDaily",
+    "1-StartDaily",
     schedule_interval=None,
     catchup=False,
     default_args=default_args,
@@ -54,7 +53,7 @@ with DAG(
     # },
 ) as dag:
 
-    @task(provide_context=True)
+    @task
     def get_integration_ids():
         try:
             # Conecte-se ao PostgreSQL e execute o script
@@ -62,7 +61,7 @@ with DAG(
             query = """
             select distinct id from public.integrations_integration
             where is_active = true 
-            and infra_create_status = true limit 1
+            and infra_create_status = true limit 2
             """
             integration_ids = hook.get_records(query)
             return [integration[0] for integration in integration_ids]
@@ -73,19 +72,31 @@ with DAG(
             )
             return []
 
-    @task
-    def trigger_import_dag(integration_id):
-        
-        return TriggerDagRunOperator(
-            task_id=f"trigger_dag_imports_{integration_id}",
-            trigger_dag_id="1-ImportVtex-Brands-Categories-Skus-Products",
-            conf={
+    def trigger_dag_run(dag_id, conf, execution_date=None, replace_microseconds=False):
+        trigger_dag(
+            dag_id=dag_id,
+            run_id=f"manual__{datetime.utcnow().isoformat()}",
+            conf=conf,
+            execution_date=execution_date,
+            replace_microseconds=replace_microseconds,
+        )
+
+    def trigger_dag_run_task(integration_ids):
+        for integration_id in integration_ids:
+            conf = {
                 "PGSCHEMA": integration_id,
                 "ISDAILY": False
             }
-        ).execute(context=integration_id)
+            trigger_dag_run(
+                dag_id="1-ImportVtex-Brands-Categories-Skus-Products",
+                conf=conf
+            )
 
-    integration_ids = get_integration_ids()
+    # Crie a tarefa Python para disparar a DAG
+    trigger_task = PythonOperator(
+        task_id="trigger_import_dags",
+        python_callable=trigger_dag_run_task,
+        op_args=[get_integration_ids()],
+    )
 
-    # Usando dynamic task mapping para criar tarefas dinamicamente
-    trigger_import_dag.expand(integration_id=integration_ids)
+    get_integration_ids() >> trigger_task
