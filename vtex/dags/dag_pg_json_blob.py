0dc2a82b399682c747a54667219bfefe6f6a3620
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-08-16 22:12:21 -0300

    aaaa

--- vtex/dags/dag_pg_json_blob.py
+++ vtex/dags/dag_pg_json_blob.py
@@ -99,14 +99,14 @@ def upload_to_blob_directory(ti):#,file_name,pg_schema):
     output_filepath = ti.xcom_pull(task_ids='extract_postgres_to_json')
     wasb_hook = WasbHook(wasb_conn_id='azure_blob_storage_json')
     #blob_name=f"{pg_schema}/{file_name}.json" 
-    blob_name = "a5be7ce1-ce65-46f8-a293-4efff72819ce/grafico.json"
+    blob_name = 'grafico.json'
 
      ###   Verifica se o arquivo já existe
     if wasb_hook.check_for_blob(container_name="jsondashboard", blob_name=blob_name):
         wasb_hook.delete_file(container_name="jsondashboard", blob_name=blob_name)
 
     upload_task = LocalFilesystemToWasbOperator(
-        task_id=f'upload_to_blob_grafico',
+        task_id=f'upload_to_blob_graficooooo',
         file_path=output_filepath,  # O arquivo JSON gerado na tarefa anterior
         container_name='jsondashboard',  # Substitua pelo nome do seu container no Azure Blob Storage
       #  blob_name=directory_name + 'postgres_data.json',  # Nome do arquivo no Blob Storage dentro do diretório
