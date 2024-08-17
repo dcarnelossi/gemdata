39bfc5cec91259acffaabf446781ec33cbb44b5b
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-08-16 22:34:28 -0300

    aa

--- vtex/dags/dag_pg_json_blob.py
+++ vtex/dags/dag_pg_json_blob.py
@@ -47,7 +47,7 @@ def extract_postgres_to_json(sql_script,file_name,pg_schema):
 
             cursor.execute(sql_script)
 
-            print("sql_script")    
+          
             records = cursor.fetchall()
             colnames = [desc[0] for desc in cursor.description]
             
@@ -58,14 +58,14 @@ def extract_postgres_to_json(sql_script,file_name,pg_schema):
             json_data = json.dumps(data, indent=4)
             print(json_data)
             # Criando um diretório temporário para armazenar o arquivo JSON
-           # tmp_dir = os.path.join(f"/tmp/{pg_schema}/" )  # Gera um diretório temporário único
-            tmp_dir = os.path.join("/tmp/" )
+            tmp_dir = os.path.join(f"/tmp/{pg_schema}/" )  # Gera um diretório temporário único
+            #tmp_dir = os.path.join("/tmp/" )
             os.makedirs(tmp_dir, exist_ok=True)  # Garante que o diretório exista
    
             # Definindo o caminho completo para o arquivo JSON
             #output_filepath = os.path.join(tmp_dir, f"{file_name}.json")
         
-            output_filepath = os.path.join(tmp_dir, "grafico.json")
+            output_filepath = os.path.join(tmp_dir, file_name)
             
             # Salvando o JSON string em um arquivo temporário
             with open(output_filepath, 'w') as outfile:
@@ -98,7 +98,7 @@ def upload_to_blob_directory(ti,file_name,pg_schema):
 
     upload_task = LocalFilesystemToWasbOperator(
         task_id=f'upload_to_blob_grafico',
-        file_path="/tmp/grafico.json",  # O arquivo JSON gerado na tarefa anterior
+        file_path="/tmp/a5be7ce1-ce65-46f8-a293-4efff72819ce/grafico.json",  # O arquivo JSON gerado na tarefa anterior
         container_name='jsondashboard',  # Substitua pelo nome do seu container no Azure Blob Storage
       #  blob_name=directory_name + 'postgres_data.json',  # Nome do arquivo no Blob Storage dentro do diretório
         blob_name= blob_name,
