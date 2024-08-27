d1f81e0e37ac9a74ef62d221acc9827d82ccce5e
Daniel Carnelossi <dcarnelossi@gmail.com>
2024-08-27 19:23:15 -0300

    -

--- vtex/dags/dag_testeblob.py
+++ vtex/dags/dag_testeblob.py
@@ -8,14 +8,22 @@ def test_blob_storage_connection():
     # Nome da conexão configurada no Airflow
     conn_id = "airflowlogs"
 
-    # Inicializa o WasbHook com o ID da conexão
-    wasb_hook = WasbHook(wasb_conn_id=conn_id)
+    try:
+        # Inicializa o WasbHook com o ID da conexão
+        wasb_hook = WasbHook(wasb_conn_id=conn_id)
 
-    # Tenta listar os containers no Blob Storage
-    containers = wasb_hook.connection.list_containers()
-    print(
-        f"Conexão bem-sucedida! Containers disponíveis: {[container.name for container in containers]}"
-    )
+        # Estabelece a conexão
+        blob_service_client = wasb_hook.get_conn()
+
+        # Tenta listar os containers no Blob Storage
+        containers = blob_service_client.list_containers()
+        print(
+            f"Conexão bem-sucedida! Containers disponíveis: {[container.name for container in containers]}"
+        )
+    except Exception as e:
+        # Se ocorrer um erro, ele será capturado aqui
+        print(f"Erro ao tentar se conectar ao Azure Blob Storage: {str(e)}")
+        raise
 
 
 # Configuração da DAG
