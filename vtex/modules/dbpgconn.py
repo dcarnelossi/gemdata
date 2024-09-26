31da9c481b5bcee83277aabaf703c9d8081dad5c
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-09-25 23:27:14 -0300

    aaa

--- vtex/modules/dbpgconn.py
+++ vtex/modules/dbpgconn.py
@@ -6,9 +6,9 @@ from datetime import datetime
 import psycopg2
 from dotenv import load_dotenv
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.patches as patches
+# import numpy as np
 
 
 class PostgresConnection:
