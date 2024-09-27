f59cff0b1203041d3cdae69e74d70152631a070e
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-09-27 18:13:34 -0300

    a

--- vtex/dags/dag_report_send_pdf .py
+++ vtex/dags/dag_report_send_pdf .py
@@ -73,7 +73,7 @@ with DAG(
             description="Enter com False (processo total) ou True (processo diario) .",
             section="Important params",
             enum=["1_relatorio_mensal", "2_relatorio_semanal","3_relatorio_personalizado"],  # Opções para o dropdown
-     #       default=None,  # Valor padrão selecionado
+            default=None,  # Valor padrão selecionado
         )
 
     },
