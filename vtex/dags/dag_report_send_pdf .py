391ebde9a7259afbcfccbcb949ff42432addcd22
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-09-27 18:55:40 -0300

    aaa

--- vtex/dags/dag_report_send_pdf .py
+++ vtex/dags/dag_report_send_pdf .py
@@ -155,26 +155,26 @@ with DAG(
 
     # @task(provide_context=True)
     # def executar_report(**kwargs):
-    enviaremail=dag.params["SENDEMAIL"]
-
    
-    if enviaremail:
-        listemail=report_baixar_email()
-        filepdf=report_baixar_pdf()
-        tiporelatorio=dag.params["TYPREREPORT"]
-        if tiporelatorio== '1_relatorio_mensal':
-            print("ok")
-            send_email_task =report_send_email_pdf(listemail,"Relatório Mensal","<p>Segue anexo o relatório mensal.</p>",filepdf) 
-            listemail >> filepdf >> send_email_task
-        elif  tiporelatorio== '2_relatorio_semanal':  
-            print("ok")
-            report_send_email_pdf(listemail,"Relatório Semanal","<p>Segue anexo o relatório Semanal.</p>",filepdf)       
-        elif  tiporelatorio== '3_relatorio_personalizado':   
+
+    def decide_process(**kwargs):
+        enviaremail=  kwargs["params"]["SENDEMAIL"]
+        if enviaremail:
+            listemail=report_baixar_email()
+            filepdf=report_baixar_pdf()
+            tiporelatorio= kwargs["params"]["TYPREREPORT"]
+            if tiporelatorio== '1_relatorio_mensal':
                 print("ok")
-    else:
-        print("aaaaaaaaaaaaaa")
-        print(enviaremail)
-        report_baixar_email()
+                send_email_task =report_send_email_pdf(listemail,"Relatório Mensal","<p>Segue anexo o relatório mensal.</p>",filepdf) 
+                listemail >> filepdf >> send_email_task
+            elif  tiporelatorio== '2_relatorio_semanal':  
+                print("ok")
+                report_send_email_pdf(listemail,"Relatório Semanal","<p>Segue anexo o relatório Semanal.</p>",filepdf)       
+            elif  tiporelatorio== '3_relatorio_personalizado':   
+                    print("ok")
+        else:
+            print("aaaaaaaaaaaaaa")
+       
 
 
     
