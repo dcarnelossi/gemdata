94060a6fa5ad66d3d19d9c790e8a5677e9f625a0
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-09-27 14:35:15 -0300

    a

--- vtex/dags/dag_report_pdf_2.py
+++ vtex/dags/dag_report_pdf_2.py
@@ -165,7 +165,7 @@ with DAG(
             # Coloque a lógica do relatório semanal aqui
         elif tiporela == "2_relatorio_semanal":
             from modules import report_weekly
-            semana = data_ini.strftime("%W")
+            semana = data_ini.strftime("%U")
             print(semana)
 
             try:
