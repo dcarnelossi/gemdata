7f56a1dcff8ab31e5b159472c4024bcb54ee4516
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-09-28 11:06:00 -0300

    a

--- vtex/dags/dag_report_pdf.py
+++ vtex/dags/dag_report_pdf.py
@@ -164,7 +164,7 @@ with DAG(
             # Coloque a lógica do relatório semanal aqui
         elif tiporela == "2_relatorio_semanal":
             from modules import report_weekly
-            semana = data_ini.strftime("%W")+1
+            semana = int(data_ini.strftime("%W"))+1
             print(semana)
             caminho_pdf= f"relatorio_semanal_{semana}_{celphone}_{numeric_datetime}"
             try:
