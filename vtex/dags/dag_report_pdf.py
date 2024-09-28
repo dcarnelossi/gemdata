6f9b72f572b3145375557bcc92f1159bfbfcd21c
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-09-28 10:49:04 -0300

    aa

--- vtex/dags/dag_report_pdf.py
+++ vtex/dags/dag_report_pdf.py
@@ -6,6 +6,8 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.models.param import Param
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonVirtualenvOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from modules.dags_common_functions import (
@@ -36,7 +38,7 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     default_args=default_args,
-    tags=["vtex", "v2", "report"],
+    tags=["vtex", "v1", "report"],
     render_template_as_native_obj=True,
     params={
         "PGSCHEMA": Param(
@@ -91,137 +93,136 @@ with DAG(
     },
 ) as dag:
 
-    def dag_report_pdf():
+    @task(provide_context=True)
+    def report_pdf(**kwargs):
+        try:
+            team_id = kwargs["params"]["PGSCHEMA"]
+            tiporela = kwargs["params"]["TYPREREPORT"]
+            celphone = kwargs["params"]["CELULAR"]
+            data_ini = datetime.strptime(kwargs["params"]["DATAINI"],"%Y-%m-%d")
+            data_fim = datetime.strptime(kwargs["params"]["DATAFIM"],"%Y-%m-%d")
+            isemail = kwargs["params"]["SENDEMAIL"] 
+            caminho_pdf =""
+            print(team_id)
+            print(tiporela)
+            print(celphone)
+            print(data_ini)
+            print(data_fim)
+            print(isemail)
 
-        @task(provide_context=True)
-        def report_pdf(**kwargs):
+            current_datetime = datetime.now() 
+            numeric_datetime = current_datetime.strftime('%Y%m%d%H%M%S')
+
+            data_conection_info = get_data_conection_info(team_id)
+        except Exception as e:
+            logging.exception(f"erro nos paramentos - {e}")
+        
+        try:
+            # Conecte-se ao PostgreSQL e execute o script
+            hook = PostgresHook(postgres_conn_id="appgemdata-dev")
+            query = f"""
+                select distinct 
+                te.logo as team_logo
+                from integrations_integration ii 
+                inner join public.teams_team te on 
+                te.ID = ii.team_id
+                where 
+                ii.id = '{team_id}'
+                and 
+                ii.infra_create_status =  true 
+                and 
+                ii.is_active = true 
+                """
+        
+            resultado_logo = hook.get_records(query)
+      
+            caminho_logo = resultado_logo[0][0] 
+        except Exception as e:
+            logging.exception(f"deu erro ao achar o caminho do logo - {e}")
+            
+        # Lógica condicional com base na escolha do usuário
+        if tiporela == "1_relatorio_mensal":
+            from modules import report_month
+            mes = data_ini.month 
+            print(mes)   
+
+            caminho_pdf= f"relatorio_mensal_{mes}_{celphone}_{numeric_datetime}"
             try:
-                team_id = kwargs["params"]["PGSCHEMA"]
-                tiporela = kwargs["params"]["TYPREREPORT"]
-                celphone = kwargs["params"]["CELULAR"]
-                data_ini = datetime.strptime(kwargs["params"]["DATAINI"],"%Y-%m-%d")
-                data_fim = datetime.strptime(kwargs["params"]["DATAFIM"],"%Y-%m-%d")
-                isemail = kwargs["params"]["SENDEMAIL"] 
-                caminho_pdf =""
-                print(team_id)
-                print(tiporela)
-                print(celphone)
-                print(data_ini)
-                print(data_fim)
-                print(isemail)
-
-                current_datetime = datetime.now() 
-                numeric_datetime = current_datetime.strftime('%Y%m%d%H%M%S')
-
-                data_conection_info = get_data_conection_info(team_id)
+                print("Processando o Relatório mensal...")
+                report_month.set_globals(
+                data_conection_info,
+                team_id,
+                celphone,
+                mes,
+                caminho_logo,
+                caminho_pdf 
+                )
+                print("Relatório mensal processado...")
             except Exception as e:
-                logging.exception(f"erro nos paramentos - {e}")
-            
+                logging.exception(f"Erro ao processar o relatorio mensal - {e}")
+                raise
+            # Coloque a lógica do relatório semanal aqui
+        elif tiporela == "2_relatorio_semanal":
+            from modules import report_weekly
+            semana = data_ini.strftime("%W")+1
+            print(semana)
+            caminho_pdf= f"relatorio_semanal_{semana}_{celphone}_{numeric_datetime}"
             try:
-                # Conecte-se ao PostgreSQL e execute o script
-                hook = PostgresHook(postgres_conn_id="appgemdata-dev")
-                query = f"""
-                    select distinct 
-                    te.logo as team_logo
-                    from integrations_integration ii 
-                    inner join public.teams_team te on 
-                    te.ID = ii.team_id
-                    where 
-                    ii.id = '{team_id}'
-                    and 
-                    ii.infra_create_status =  true 
-                    and 
-                    ii.is_active = true 
-                    """
-            
-                resultado_logo = hook.get_records(query)
-        
-                caminho_logo = resultado_logo[0][0] 
+                print("Processando o Relatório  semanal...")
+                report_weekly.set_globals(
+                data_conection_info,
+                team_id,
+                celphone,
+                semana,
+                caminho_logo,
+                caminho_pdf
+                )
+                print("Relatório semanal processado...")
+           
             except Exception as e:
-                logging.exception(f"deu erro ao achar o caminho do logo - {e}")
-                
-            # Lógica condicional com base na escolha do usuário
-            if tiporela == "1_relatorio_mensal":
-                from modules import report_month
-                mes = data_ini.month 
-                print(mes)   
+                logging.exception(f"Erro ao processar o relatorio semanal - {e}")
+                raise
+                  
+        elif tiporela == "3_relatorio_personalizado":
+            print("Processando o Relatório Diário...")
+            # Coloque a lógica do relatório diário aqui
+             
+        else:
+            print("Opção de relatório desconhecida.")
+     
+        return caminho_pdf
 
-                caminho_pdf= f"relatorio_mensal_{mes}_{celphone}_{numeric_datetime}"
-                try:
-                    print("Processando o Relatório mensal...")
-                    report_month.set_globals(
-                    data_conection_info,
-                    team_id,
-                    celphone,
-                    mes,
-                    caminho_logo,
-                    caminho_pdf 
-                    )
-                    print("Relatório mensal processado...")
-                except Exception as e:
-                    logging.exception(f"Erro ao processar o relatorio mensal - {e}")
-                    raise
-                # Coloque a lógica do relatório semanal aqui
-            elif tiporela == "2_relatorio_semanal":
-                from modules import report_weekly
-                semana = data_ini.strftime("%W")+1
-                print(semana)
-                caminho_pdf= f"relatorio_semanal_{semana}_{celphone}_{numeric_datetime}"
-                try:
-                    print("Processando o Relatório  semanal...")
-                    report_weekly.set_globals(
-                    data_conection_info,
-                    team_id,
-                    celphone,
-                    semana,
-                    caminho_logo,
-                    caminho_pdf
-                    )
-                    print("Relatório semanal processado...")
-            
-                except Exception as e:
-                    logging.exception(f"Erro ao processar o relatorio semanal - {e}")
-                    raise
-                    
-            elif tiporela == "3_relatorio_personalizado":
-                print("Processando o Relatório Diário...")
-                # Coloque a lógica do relatório diário aqui
-                
-            else:
-                print("Opção de relatório desconhecida.")
-        
-            return caminho_pdf
-
-        @task.branch
-        def should_trigger_dag(params):
-        # Substitua `params['YOUR_PARAM']` pela condição que você quer verificar
-            if params['TYPREREPORT']:  # Troque YOUR_PARAM pelo nome do parâmetro que você deseja verificar
-                return 'trigger_dag_report_send_pdf'
-            else:
-                return 'skip_trigger'
+    @task.branch
+    def should_trigger_dag(params):
+    # Substitua `params['YOUR_PARAM']` pela condição que você quer verificar
+        if params['TYPREREPORT']:  # Troque YOUR_PARAM pelo nome do parâmetro que você deseja verificar
+            return 'trigger_dag_report_send_pdf'
+        else:
+            return 'skip_trigger'
 
 
-        cam_pdf = report_pdf()
-        
-        @task
-        def skip_trigger():
-            print("Condição não atendida, a DAG não será disparada")
+    cam_pdf = report_pdf()
     
-        #@task(provide_context=True)   
-        trigger_dag_report_send_pdf = TriggerDagRunOperator(
-            task_id="trigger_dag_report_send_pdf",
-            trigger_dag_id="b2-report-send-pdf",  # Substitua pelo nome real da sua segunda DAG
-            conf={
-                    "PGSCHEMA": "{{ params.PGSCHEMA }}",
-                # "SENDEMAIL": "{{ params.SENDEMAIL }}",
-                    "FILEPDF": cam_pdf,
-                    "TYPREREPORT": "{{ params.TYPREREPORT }}"
-                }  # Se precisar passar informações adicionais para a DAG_B
-        )
-        
-        # Definindo a sequência das tasks
-        should_trigger = should_trigger_dag('{{ params }}')
-        should_trigger >> [trigger_dag_report_send_pdf, skip_trigger]
+    @task
+    def skip_trigger():
+        print("Condição não atendida, a DAG não será disparada")
+   
+    #@task(provide_context=True)   
+    trigger_dag_report_send_pdf = TriggerDagRunOperator(
+        task_id="trigger_dag_report_send_pdf",
+        trigger_dag_id="b2-report-send-pdf",  # Substitua pelo nome real da sua segunda DAG
+        conf={
+                "PGSCHEMA": "{{ params.PGSCHEMA }}",
+               # "SENDEMAIL": "{{ params.SENDEMAIL }}",
+                "FILEPDF": cam_pdf,
+                "TYPREREPORT": "{{ params.TYPREREPORT }}"
+            }  # Se precisar passar informações adicionais para a DAG_B
+    )
+    
 
-    # Chamando a DAG
-    dag_instance = dag_report_pdf()
+    should_trigger = should_trigger_dag('{{ params }}')
+    skip_trigger_task = skip_trigger()
+
+    # Definindo as dependências entre as tarefas
+    should_trigger >> trigger_dag_report_send_pdf
+    should_trigger >> skip_trigger_task
