0cd638999873c03eadcf39f1d9801bd3a179d71c
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-10-08 09:48:19 -0300

    a

--- vtex/modules/sqlscriptabglobaldemo.py
+++ vtex/modules/sqlscriptabglobaldemo.py
@@ -1,41 +1,45 @@
 def vtexsqlscriptscreatetabglobaldemo(schema):
     scripts = f"""
-                                
-            CREATE SCHEMA IF NOT EXISTS demonstracao;
+                                        
+                        CREATE SCHEMA IF NOT EXISTS demonstracao;
 
 
-            DROP TABLE IF EXISTS "demonstracao".orders_items_ia;
-            create TABLE "demonstracao".orders_items_ia
-            as
-            SELECT 
-            orderid,
-            creationdate,
-            idcat,
-            namecategory,
-            idprod,   
-            namesku,
-            sellingprice * (1.05 + (RANDOM() * (0.20))) as sellingprice ,
-            quantityorder * (2 + (RANDOM() * (0.20))) as quantityorder,
-            quantityitems * (2 + (RANDOM() * (0.20))) as quantityitems,
-            revenue_without_shipping * (1.05 + (RANDOM() * (0.20))) as revenue_without_shipping,
-            selectedaddresses_0_city,
-            selectedaddresses_0_state
-            FROM "{schema}".orders_items_ia  ;
-            
 
-            DROP TABLE IF EXISTS "demonstracao".orders_ia;
-            create TABLE "demonstracao".orders_ia
-            as
-            SELECT 
-            orderid,
-            creationdate,
-            userprofileid,
-            revenue * (1.05 + (RANDOM() * (0.20))) as revenue,
-            quantityorder * (2 + (RANDOM() * (0.20))) as quantityorder,
-            selectedaddresses_0_state,
-            selectedaddresses_0_city
+                        DROP TABLE IF EXISTS "demonstracao".orders_items_ia;
+                        create TABLE "demonstracao".orders_items_ia
+                        as
+                        SELECT 
+                        orderid,
+                        creationdate,
+                        cast(cast(idcat as integer)*2  as varchar(20)) as idcat,
+                        CONCAT('Nome Categoria ', cast(cast(idcat as integer)*2 as varchar(20))) as namecategory,
+                        cast(cast(idprod as integer)*2  as varchar(20))  as idprod,   
+                        CONCAT('Nome Produto ', cast(cast(idprod as integer)*2 as varchar(20))) as namesku,
+                        sellingprice * (1.05 + (RANDOM() * (0.20))) as sellingprice ,
+                        quantityorder * (2 + (RANDOM() * (0.20))) as quantityorder,
+                        quantityitems * (2 + (RANDOM() * (0.20))) as quantityitems,
+                        revenue_without_shipping * (1.05 + (RANDOM() * (0.20))) as revenue_without_shipping,
+                        selectedaddresses_0_city,
+                        selectedaddresses_0_state
+                        FROM "{schema}".orders_items_ia  ;
+                        
 
-            FROM "{schema}".orders_ia  ;
+                        DROP TABLE IF EXISTS "demonstracao".orders_ia;
+                        create TABLE "demonstracao".orders_ia
+                        as
+                        SELECT 
+                        orderid,
+                        creationdate,
+                        userprofileid,
+                        revenue * (1.05 + (RANDOM() * (0.20))) as revenue,
+                        quantityorder * (2 + (RANDOM() * (0.20))) as quantityorder,
+                        selectedaddresses_0_state,
+                        selectedaddresses_0_city
+
+                        FROM "{schema}".orders_ia  ;
+                        
+                        
+
                 
 
     """
