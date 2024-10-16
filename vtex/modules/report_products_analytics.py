219173838ce83dc02cff8f0e4fe86f8d1765b8f5
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-10-16 15:20:16 -0300

    a

--- vtex/modules/report_products_analytics.py
+++ vtex/modules/report_products_analytics.py
@@ -805,7 +805,7 @@ def gerar_pdf_analise(celular,integration,extensao,diretorio,caminho_pdf_blob):
     # Inserir um espaço
     pdf.ln(215)
 
-    pdf.image(f"{diretorio}/tabela_detalhada{celular}.png", x = 7,y= 50,h=400,w=195 )
+    pdf.image(f"{diretorio}/tabela_detalhada{celular}.png", x = 7,y= 50,h=300,w=195 )
 
   
         # Configurar a fonte
