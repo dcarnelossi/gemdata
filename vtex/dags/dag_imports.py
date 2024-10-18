f849191c1cc1f7f364f174c3e0934bfd523b87f4
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-10-18 17:08:48 -0300

    aa

--- vtex/dags/dag_imports.py
+++ vtex/dags/dag_imports.py
@@ -65,7 +65,7 @@ with DAG(
     @task(provide_context=True)
     def log_import_resumo(**kwargs):
         try: 
-            from modules.log_resumo_airflow import log_process
+           
             import uuid   
             report_id = str(uuid.uuid4()) 
             integration_id = kwargs["params"]["PGSCHEMA"]
@@ -87,8 +87,8 @@ with DAG(
                 
             }
             coorp_conection_info = get_coorp_conection_info()
-            
-            log_process(coorp_conection_info , data , "log_import_import", "dag_run_id")
+            from modules import log_resumo_airflow
+            log_resumo_airflow.log_process(coorp_conection_info , data , "log_import_import", "dag_run_id")
 
             logging.info(f"upserted do log diario successfully.")
         except Exception as e:
