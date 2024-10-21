e858f7608fbbe52fc22da1adddccdd131d519cc8
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-10-21 10:55:55 -0300

    aa

--- vtex/dags/dag_imports.py
+++ vtex/dags/dag_imports.py
@@ -166,143 +166,143 @@ with DAG(
         return report_id
     
     report = gerar_reportid()
-
-    log_import_task_ini = PythonOperator(
-            task_id='log_import_task_ini',
-            python_callable=log_import_pyhton,
-            op_kwargs={
-                'isfirtline':True,
-                'reportid': report,  # Defina conforme necessário
-                'erro': None,
-            },
-            provide_context=True,  # Isso garante que o contexto da DAG seja passado
-            dag=dag
-        )
-    
-      
-
-    @task(provide_context=True)
-    def brands(**kwargs):
-            integration_id = kwargs["params"]["PGSCHEMA"]
-
-            coorp_conection_info = get_coorp_conection_info()
-            data_conection_info = get_data_conection_info(integration_id)
-            api_conection_info = get_api_conection_info(integration_id)
-
-            from modules import brand
-
-            try:
-                query = """
-                UPDATE public.integrations_integrati
-                SET daily_run_date_ini = %s,
-                isdaily_manual = false
-                WHERE id = %s;
-                """
-                # Initialize the PostgresHook
-                hook2 = PostgresHook(postgres_conn_id="appgemdata-dev")
-                # Execute the query with parameters
-                
-                hook2.run(query, parameters=(datetime.now(),integration_id))
-
-
-                brand.get_brands_list_parallel(api_conection_info, data_conection_info)
-
-                # Pushing data to XCom
-                kwargs["ti"].xcom_push(key="integration_id", value=integration_id)
-                kwargs["ti"].xcom_push(
-                    key="coorp_conection_info", value=coorp_conection_info
-                )
-                kwargs["ti"].xcom_push(key="data_conection_info", value=data_conection_info)
-                kwargs["ti"].xcom_push(key="api_conection_info", value=api_conection_info)
-
-                return True
-            except Exception as e:
-                logging.exception(f"An unexpected error occurred during DAG - {e}")
-                raise e
-
-    @task(provide_context=True)
-    def categories(**kwargs):
-            ti = kwargs["ti"]
-            # integration_id = ti.xcom_pull(task_ids="brands", key="integration_id")
-            # coorp_conection_info = ti.xcom_pull(
-            #     task_ids="brands", key="coorp_conection_info"
-            # )
-            data_conection_info = ti.xcom_pull(task_ids="brands", key="data_conection_info")
-            api_conection_info = ti.xcom_pull(task_ids="brands", key="api_conection_info")
-
-            from modules import category_concurrent as category
-
-            try:
-                category.set_globals(30, api_conection_info, data_conection_info)
-                return True
-            except Exception as e:
-                logging.exception(f"An unexpected error occurred during DAG - {e}")
-                raise e
-
-    @task(provide_context=True)
-    def skus(**kwargs):
-            ti = kwargs["ti"]
-            # integration_id = ti.xcom_pull(task_ids="brands", key="integration_id")
-            # coorp_conection_info = ti.xcom_pull(
-            #     task_ids="brands", key="coorp_conection_info"
-            # )
-            data_conection_info = ti.xcom_pull(task_ids="brands", key="data_conection_info")
-            api_conection_info = ti.xcom_pull(task_ids="brands", key="api_conection_info")
-
-            from modules import sku
-
-            try:
-                sku.set_globals(1, api_conection_info, data_conection_info)
-                return True
-            except Exception as e:
-                logging.exception(f"An unexpected error occurred during DAG - {e}")
-                raise e
-
-    @task
-    def products(**kwargs):
-            ti = kwargs["ti"]
-            # integration_id = ti.xcom_pull(task_ids="brands", key="integration_id")
-            # coorp_conection_info = ti.xcom_pull(
-            #     task_ids="brands", key="coorp_conection_info"
-            # )
-            data_conection_info = ti.xcom_pull(task_ids="brands", key="data_conection_info")
-            api_conection_info = ti.xcom_pull(task_ids="brands", key="api_conection_info")
-
-            from modules import products
-
-            try:
-                products.set_globals(api_conection_info, data_conection_info)
-                return True
-            except Exception as e:
-                logging.exception(f"An unexpected error occurred during DAG - {e}")
-                raise e
+    try:   
+        log_import_task_ini = PythonOperator(
+                task_id='log_import_task_ini',
+                python_callable=log_import_pyhton,
+                op_kwargs={
+                    'isfirtline':True,
+                    'reportid': report,  # Defina conforme necessário
+                    'erro': None,
+                },
+                provide_context=True,  # Isso garante que o contexto da DAG seja passado
+                dag=dag
+            )
         
-    log_import_task_fim = PythonOperator(
-            task_id='log_import_task_fim',
-            python_callable=log_import_pyhton,
-            op_kwargs={
-                'isfirtline':False,
-                'reportid': report,  # Defina conforme necessário
-                'erro': None,
-            },
-            provide_context=True,  # Isso garante que o contexto da DAG seja passado
-            dag=dag
-        )
-    
-      
         
-    trigger_dag_orders_list = TriggerDagRunOperator(
-            task_id="trigger_dag_orders_list",
-            trigger_dag_id="2-ImportVtex-Orders-List",  # Substitua pelo nome real da sua segunda DAG
-            conf={
-                "PGSCHEMA": "{{ params.PGSCHEMA }}",
-                "ISDAILY": "{{ params.ISDAILY }}",
-                "IDREPORT": report,
-            },  # Se precisar passar informações adicionais para a DAG_B
-        )
-        # Configurando a dependência entre as tasks
 
-    try:    
+        @task(provide_context=True)
+        def brands(**kwargs):
+                integration_id = kwargs["params"]["PGSCHEMA"]
+
+                coorp_conection_info = get_coorp_conection_info()
+                data_conection_info = get_data_conection_info(integration_id)
+                api_conection_info = get_api_conection_info(integration_id)
+
+                from modules import brand
+
+                try:
+                    query = """
+                    UPDATE public.integrations_integrati
+                    SET daily_run_date_ini = %s,
+                    isdaily_manual = false
+                    WHERE id = %s;
+                    """
+                    # Initialize the PostgresHook
+                    hook2 = PostgresHook(postgres_conn_id="appgemdata-dev")
+                    # Execute the query with parameters
+                    
+                    hook2.run(query, parameters=(datetime.now(),integration_id))
+
+
+                    brand.get_brands_list_parallel(api_conection_info, data_conection_info)
+
+                    # Pushing data to XCom
+                    kwargs["ti"].xcom_push(key="integration_id", value=integration_id)
+                    kwargs["ti"].xcom_push(
+                        key="coorp_conection_info", value=coorp_conection_info
+                    )
+                    kwargs["ti"].xcom_push(key="data_conection_info", value=data_conection_info)
+                    kwargs["ti"].xcom_push(key="api_conection_info", value=api_conection_info)
+
+                    return True
+                except Exception as e:
+                    logging.exception(f"An unexpected error occurred during DAG - {e}")
+                    raise e
+
+        @task(provide_context=True)
+        def categories(**kwargs):
+                ti = kwargs["ti"]
+                # integration_id = ti.xcom_pull(task_ids="brands", key="integration_id")
+                # coorp_conection_info = ti.xcom_pull(
+                #     task_ids="brands", key="coorp_conection_info"
+                # )
+                data_conection_info = ti.xcom_pull(task_ids="brands", key="data_conection_info")
+                api_conection_info = ti.xcom_pull(task_ids="brands", key="api_conection_info")
+
+                from modules import category_concurrent as category
+
+                try:
+                    category.set_globals(30, api_conection_info, data_conection_info)
+                    return True
+                except Exception as e:
+                    logging.exception(f"An unexpected error occurred during DAG - {e}")
+                    raise e
+
+        @task(provide_context=True)
+        def skus(**kwargs):
+                ti = kwargs["ti"]
+                # integration_id = ti.xcom_pull(task_ids="brands", key="integration_id")
+                # coorp_conection_info = ti.xcom_pull(
+                #     task_ids="brands", key="coorp_conection_info"
+                # )
+                data_conection_info = ti.xcom_pull(task_ids="brands", key="data_conection_info")
+                api_conection_info = ti.xcom_pull(task_ids="brands", key="api_conection_info")
+
+                from modules import sku
+
+                try:
+                    sku.set_globals(1, api_conection_info, data_conection_info)
+                    return True
+                except Exception as e:
+                    logging.exception(f"An unexpected error occurred during DAG - {e}")
+                    raise e
+
+        @task
+        def products(**kwargs):
+                ti = kwargs["ti"]
+                # integration_id = ti.xcom_pull(task_ids="brands", key="integration_id")
+                # coorp_conection_info = ti.xcom_pull(
+                #     task_ids="brands", key="coorp_conection_info"
+                # )
+                data_conection_info = ti.xcom_pull(task_ids="brands", key="data_conection_info")
+                api_conection_info = ti.xcom_pull(task_ids="brands", key="api_conection_info")
+
+                from modules import products
+
+                try:
+                    products.set_globals(api_conection_info, data_conection_info)
+                    return True
+                except Exception as e:
+                    logging.exception(f"An unexpected error occurred during DAG - {e}")
+                    raise e
+            
+        log_import_task_fim = PythonOperator(
+                task_id='log_import_task_fim',
+                python_callable=log_import_pyhton,
+                op_kwargs={
+                    'isfirtline':False,
+                    'reportid': report,  # Defina conforme necessário
+                    'erro': None,
+                },
+                provide_context=True,  # Isso garante que o contexto da DAG seja passado
+                dag=dag
+            )
+        
+        
+            
+        trigger_dag_orders_list = TriggerDagRunOperator(
+                task_id="trigger_dag_orders_list",
+                trigger_dag_id="2-ImportVtex-Orders-List",  # Substitua pelo nome real da sua segunda DAG
+                conf={
+                    "PGSCHEMA": "{{ params.PGSCHEMA }}",
+                    "ISDAILY": "{{ params.ISDAILY }}",
+                    "IDREPORT": report,
+                },  # Se precisar passar informações adicionais para a DAG_B
+            )
+            # Configurando a dependência entre as tasks
+
+    
         brands_task = brands()
         categories_task = categories()
         sku_task = skus()
