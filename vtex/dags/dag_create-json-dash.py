d862ef239d566e1f25c40eeea4751d2a27a7c93d
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-10-29 12:14:37 -0300

    tirando reprocessar

--- vtex/dags/dag_create-json-dash.py
+++ vtex/dags/dag_create-json-dash.py
@@ -39,8 +39,7 @@ default_args = {
     "start_date": datetime(2024, 1, 1),
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 3,  # Tentativas de reexecução
-    "retry_delay": timedelta(minutes=1),  # Intervalo entre tentativas
+
 
 }
 
