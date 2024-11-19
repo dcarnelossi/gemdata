440d21fbb4dd47d63d4b5782ac63a473a6477ab0
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-11-19 17:31:45 -0300

    prodwhats

--- vtex/modules/save_to_blob.py
+++ vtex/modules/save_to_blob.py
@@ -4,7 +4,7 @@ from azure.storage.blob import BlobServiceClient
 class ExecuteBlob:
     def __init__(self):
         # Obter a conexão cadastrada no Airflow
-        connection = BaseHook.get_connection('azure_blob_storage_json')
+        connection = BaseHook.get_connection('appgemdata-storage-prod')
 
         # Obter a connection string da conexão cadastrada
         connection_string = connection.extra_dejson.get('connection_string')
