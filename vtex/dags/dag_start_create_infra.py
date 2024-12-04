bd0f90d9087d5933618841202be14f331cfd7c91
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-12-04 11:03:56 -0300

    a

--- vtex/dags/dag_start_create_infra.py
+++ vtex/dags/dag_start_create_infra.py
@@ -26,73 +26,74 @@ with DAG(
     tags=["Start-CreateInfra", "v2", "Schedule"],
     render_template_as_native_obj=True,
 ) as dag:
+    def create_infra_dag():
 
-    @task(provide_context=True)
-    def get_integration_id(**context):
-        try:
-            # Conecte-se ao PostgreSQL e execute o script
-            hook = PostgresHook(postgres_conn_id="appgemdata-pgserver-prod")
-            query = """
-            select distinct id from public.integrations_integration
-            where is_active = true 
-            and infra_create_status = false
-            limit 1;
-            """
-            integration_ids = hook.get_records(query)
-            if not integration_ids:
-                print("Nenhuma integração para criar infraestrutura")
-                return None
+        @task()
+        def get_integration_id():
+            try:
+                # Conecte-se ao PostgreSQL e execute o script
+                hook = PostgresHook(postgres_conn_id="appgemdata-pgserver-prod")
+                query = """
+                SELECT DISTINCT id, hosting 
+                FROM public.integrations_integration
+                WHERE is_active = TRUE 
+                AND infra_create_status = FALSE
+                LIMIT 1;
+                """
+                integration_ids = hook.get_records(query)
+                if not integration_ids:
+                    print("Nenhuma integração para criar infraestrutura")
+                    return None
 
-            integration_id = integration_ids[0][0]
-            print(f"Iniciando criação de infraestrutura para integração: {integration_id}")
-           
-            
-           
-           
-            return integration_id
+                integration_id = integration_ids[0]
+                print(f"Iniciando criação de infraestrutura para integração: {integration_id}")
+                return {"id": integration_id[0], "hosting": integration_id[1]}
 
-        except Exception as e:
-            logging.exception(
-                f"An unexpected error occurred during get_integration_id - {e}"
+            except Exception as e:
+                logging.exception(
+                    f"An unexpected error occurred during get_integration_id - {e}"
+                )
+                raise
+
+        def choose_next_step(ti, **context):
+            integration_data = ti.xcom_pull(task_ids="get_integration_id")
+            if integration_data:
+                return "trigger_dag_crete_infra"
+            else:
+                return "no_integration_ids"
+
+        def trigger_dag_run(ti, **context):
+            integration_data = ti.xcom_pull(task_ids="get_integration_id")
+            integration_id = integration_data["id"]
+            hosting = integration_data["hosting"]
+
+            trigger = TriggerDagRunOperator(
+                task_id=f"dev-0-CreateInfra-{integration_id}",
+                trigger_dag_id="dev-0-CreateInfra",  # Substitua pelo nome real da sua segunda DAG
+                conf={
+                    "PGSCHEMA": integration_id,
+                    "ISDAILY": False,
+                    "HOSTING": hosting
+                },
             )
-            raise
+            trigger.execute(context=context)
 
-    def choose_next_step(integration_id, **context):
-        if integration_id:
-            return "trigger_dag_crete_infra"
-        else:
-            return "no_integration_ids"
+        integration_id_task = get_integration_id()
 
-    def trigger_dag_run(integration_id, **context):
-        return TriggerDagRunOperator(
-            task_id=f"0-CreateInfra-{integration_id}",
-            trigger_dag_id="0-CreateInfra",  # Substitua pelo nome real da sua segunda DAG
-            conf={
-                "PGSCHEMA": integration_id,
-                "ISDAILY": False              
-            },
-        ).execute(context=context)
+        next_step = BranchPythonOperator(
+            task_id="check_integration_id",
+            python_callable=choose_next_step,
+        )
 
-    integration_id = get_integration_id()
+        trigger_dag = PythonOperator(
+            task_id="trigger_dag_crete_infra",
+            python_callable=trigger_dag_run,
+        )
 
-    next_step = BranchPythonOperator(
-        task_id="check_integration_id",
-        python_callable=choose_next_step,
-        op_args=[integration_id],
-        provide_context=True,
-    )
+        no_integration_ids = EmptyOperator(task_id="no_integration_ids")
 
-    trigger_dag = PythonOperator(
-        task_id="trigger_dag_crete_infra",
-        python_callable=trigger_dag_run,
-        op_args=[integration_id],
-        provide_context=True,
-    )
+        integration_id_task >> next_step
+        next_step >> [trigger_dag, no_integration_ids]
 
-    no_integration_ids = EmptyOperator(
-        task_id="no_integration_ids"
-    )
 
-    integration_id >> next_step
-    next_step >> trigger_dag
-    next_step >> no_integration_ids
+    dag_instance = create_infra_dag()
