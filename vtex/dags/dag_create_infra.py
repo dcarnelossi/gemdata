fee62b7ab1b0f0a1b422837f0cf8bb4ae587e93b
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-12-04 08:44:35 -0300

    a

--- vtex/dags/dag_create_infra.py
+++ vtex/dags/dag_create_infra.py
@@ -6,6 +6,7 @@ from airflow.decorators import task
 from airflow.models.param import Param
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import BranchPythonOperator
 
 # Lista de requisitos
 requirements = [
@@ -54,16 +55,20 @@ with DAG(
     @task(provide_context=True)
     def create_postgres_infra(**kwargs):
         PGSCHEMA = kwargs["params"]["PGSCHEMA"]
+        HOSTING = kwargs["params"]["HOSTING"]
 
-        from modules.sqlscripts import vtexsqlscripts
+        import modules.sqlscripts as scripts
 
         try:
+            
+            
             # Defina o código SQL para criar a tabela
-            sql_script = vtexsqlscripts(PGSCHEMA, "adminuserapppggemdataprod")
+            if(HOSTING == 'vtex'):
+                sql_script = scripts.vtexsqlscripts(PGSCHEMA, "adminuserapppggemdataprod")
+            else:
+                sql_script = scripts.shopifysqlscripts(PGSCHEMA, "adminuserapppggemdataprod")
 
-            # Conecte-se ao PostgreSQL e execute o script
-            # TODO postgres_conn_id deve ser uma variavel vinda da chamada da DAG
-            # não pode estar cravada aqui no codigo
+           
             hook = PostgresHook(postgres_conn_id="integrations-pgserver-prod")
             hook.run(sql_script)
             
@@ -74,7 +79,7 @@ with DAG(
             """
 
             # Initialize the PostgresHook
-            hook2 = PostgresHook(postgres_conn_id="appgemdata-pgserver-prod")
+            hook2 = PostgresHook(postgres_conn_id="appgemdata-homol")
 
             # Execute the query with parameters
             hook2.run(query, parameters=(datetime.now(), PGSCHEMA))
@@ -86,19 +91,49 @@ with DAG(
                 f"An unexpected error occurred during create_postgres_infra - {e}"
             )
             return e
+    
+    
+    def choose_trigger_dag(**kwargs):
+        hosting = kwargs["params"]["HOSTING"]
+          # Adicione o log aqui
+        #logging.info(f"Escolhido o branch com base no HOSTING: {hosting}")
+        if hosting.lower() == "vtex":
+            return 'trigger_vtex_import'
+        else: 
+            return 'trigger_shopify_orders_import'
 
-    trigger_dag_imports = TriggerDagRunOperator(
-        task_id="trigger_dag_imports",
-        trigger_dag_id="1-ImportVtex-Brands-Categories-Skus-Products",  # Substitua pelo nome real da sua segunda DAG
+    branch_task = BranchPythonOperator(
+        task_id='choose_trigger_dag',
+        provide_context=True,
+        python_callable=choose_trigger_dag
+    )
+
+
+
+    # Trigger para VTEX
+    trigger_vtex_import_ini = TriggerDagRunOperator(
+        task_id="trigger_vtex_import",
+        trigger_dag_id="1-ImportVtex-Brands-Categories-Skus-Products",
         conf={
             "PGSCHEMA": "{{ params.PGSCHEMA }}",
-            "ISDAILY": "{{ params.ISDAILY }}"
-                
-        },  # Se precisar passar informações adicionais para a DAG_B
+            "ISDAILY": "{{ params.ISDAILY }}",
+        },
+    )
+
+    # Trigger para Shopify
+    trigger_shopify_orders_import_ini = TriggerDagRunOperator(
+        task_id="trigger_shopify_orders_import",
+        trigger_dag_id="shopify-1-Orders",
+        conf={
+            "PGSCHEMA": "{{ params.PGSCHEMA }}",
+            "ISDAILY": "{{ params.ISDAILY }}",
+        },
     )
 
     # Configurando a dependência entre as tarefas
-
     create_postgres_infra_task = create_postgres_infra()
+   # choose_trigger_dag_task = choose_trigger_dag()
 
-    create_postgres_infra_task >> trigger_dag_imports
+
+    create_postgres_infra_task >> branch_task >> [trigger_vtex_import_ini, trigger_shopify_orders_import_ini]
+
