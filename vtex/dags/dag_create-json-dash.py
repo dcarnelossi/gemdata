dcd15b9d3490474c9331bc7b9d5029fd763c07ee
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2024-12-18 15:27:41 -0300

    a

--- vtex/dags/dag_create-json-dash.py
+++ vtex/dags/dag_create-json-dash.py
@@ -199,7 +199,10 @@ def post_analytics_analytics(pg_schema):
                           ('revenue','faturamento_regiao.json','cities'),
                           ('revenue','faturamento_compradores.json','buyers'),
                           ('revenue','faturamento_mensal.json','revenuemensal'),
+                          ('revenue','pedido_por_categoria.json','pcategory'),
+                          ('revenue','pedido_por_estado.json','pcities'),
                           ('products','pedido_ecommerce.json','products')
+
                           ]
             distinct_first_column = set(aba[0] for aba in aba_dash) 
 
