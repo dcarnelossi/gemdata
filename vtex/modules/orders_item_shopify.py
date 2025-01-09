732011e449c0822e12f0fab8dd82820fc915ca67
Gabriel Sousa <gabriel.pereira.sousa@gmail.com>
2025-01-09 15:25:38 -0300

    a

--- vtex/modules/orders_item_shopify.py
+++ vtex/modules/orders_item_shopify.py
@@ -171,7 +171,8 @@ def get_orders_ids_from_db(start_date=None):
                 ON  oi.orderid = so.orderid
                 where cancelreason is null and so.currentsubtotalprice <>0
                 GROUP BY so.orderid
-                HAVING max(so.currentsubtotalprice +so.currenttotaldiscounts  ) <> COALESCE(SUM(oi.originalunitprice*oi.quantity),0.00);  
+                HAVING max(so.currentsubtotalprice +so.currenttotaldiscounts  ) <> COALESCE(SUM(oi.originalunitprice*oi.quantity),0.00)  
+				and max(so.currentsubtotalprice ) <> COALESCE(SUM((oi.originalunitprice*oi.quantity)-totaldiscountamount ),0.00);                
                 """ 
            
         else:
